553165a8233322e23b4be19e08deef2e4db2433a
liuleigang <liuleigang@gmail.com>
2018-11-01 10:50:51 +0800

    update

--- AudioPlayer/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ AudioPlayer/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -51,12 +33,14 @@ var app = {
     }
 };
 
+// init, add DeviceReady listener
 app.initialize();
 
 function onBatteryStatus(info) {
    alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
 
+//
 var myMedia = null;
 
 function playAudio() {
@@ -72,7 +56,6 @@ function playAudio() {
       function onError(error) {
          console.log("playAudio Error: " + error.code);
       }
-
    }
 
    myMedia.play();
@@ -88,18 +71,17 @@ function stopAudio() {
    if(myMedia) {
       myMedia.stop(); 
    }
-	
    myMedia = null;
 }
 
 var volumeValue = 0.5;
-
+// volume up
 function volumeUp() {
    if(myMedia && volumeValue < 1) {
       myMedia.setVolume(volumeValue += 0.1);
    }
 }
-
+// volume down
 function volumeDown() {
    if(myMedia && volumeValue > 0) {
       myMedia.setVolume(volumeValue -= 0.1);
